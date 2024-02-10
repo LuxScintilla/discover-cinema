fdf3a88f56d3be98ae90bf5e59f8780c5f616520
LuxScintilla <105556122+LuxScintilla@users.noreply.github.com>
2024-02-10 22:19:42 +0000

    initial commit

---
+++ functions/fetch-movie/fetch-movie.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+const handler = async (event) => {
+  const API_KEY = process.env.API_KEY;
+  const { query } = event.queryStringParameters;
+  let URL;
+  if (query) {
+    URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+  } else {
+    URL = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${API_KEY}`;
+  }
+
+  try {
+    const { data } = await axios.get(URL);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    const { status, statusText, headers, data } = error.response;
+    return {
+      statusCode: status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    };
+  }
+};
+
+module.exports = { handler };
---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+// const mySearch = "avatar";
+// const imgPath = "https://image.tmdb.org/t/p/original";
+
+// fetch(`/.netlify/functions/fetch-movie?query=${mySearch}`).then((response) =>
+//   console.log(response.json())
+// );
+
+// fetch(`/.netlify/functions/fetch-movie`).then((response) =>
+//   console.log(response.json())
+// );
+
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
+    return;
+  }
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) {
+      return;
+    }
+    dropdown.classList.remove("active");
+  });
+});
+
+const loadBackground = async function () {
+  const imgPath = "https://image.tmdb.org/t/p/original";
+  try {
+    const response = await fetch(`/.netlify/functions/fetch-movie`);
+    const movies = await response.json();
+
+    document.body.style.backgroundImage = `linear-gradient(to bottom right, rgba(19, 20, 43, 0.6), rgba(0, 0, 0, 0.8)), url(${imgPath}${movies.results[8].backdrop_path})`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadBackground();
