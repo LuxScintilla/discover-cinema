dbea94f2a91c2af7d6ede3605d64536a111ac99c
LuxScintilla <105556122+LuxScintilla@users.noreply.github.com>
2024-02-11 12:49:27 +0000

    add movie data to page

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,36 @@
 //   console.log(response.json())
 // );
 
+// --------- GENRE ID FROM API ---------
+
+const genreID = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  crime: 80,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  science_fiction: 878,
+  tv_movie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37,
+};
+
+// --------- DROPDOWN MENU ---------
+
 document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  const caret = document.querySelector(".fa-caret-down");
+
   if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
     return;
   }
@@ -19,6 +47,7 @@ document.addEventListener("click", (e) => {
   if (isDropdownButton) {
     currentDropdown = e.target.closest("[data-dropdown]");
     currentDropdown.classList.toggle("active");
+    caret.classList.toggle("fa-caret-down__rotate");
   }
 
   document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
@@ -26,19 +55,58 @@ document.addEventListener("click", (e) => {
       return;
     }
     dropdown.classList.remove("active");
+    caret.classList.remove("fa-caret-down__rotate");
   });
 });
 
-const loadBackground = async function () {
+// --------- HELPER FUNCTIONS ---------
+
+const firstLetterCapitalize = function (word) {
+  const firstLetter = word.charAt(0).toUpperCase();
+  const remaining = word.slice(1);
+  return `${firstLetter}${remaining}`;
+};
+
+// --------- LOAD INITIAL FEATURED TRENDING FROM API BY RANDOM ---------
+
+const loadInitial = async function () {
+  const featuredTitle = document.querySelector(".featured__title");
+  const featuredDate = document.querySelector(".featured__date");
+  const featuredGenre = document.querySelector(".featured__genre");
+  const featuredOverview = document.querySelector(".featured__overview");
+
+  const random = Math.floor(Math.random() * 20);
   const imgPath = "https://image.tmdb.org/t/p/original";
+
   try {
     const response = await fetch(`/.netlify/functions/fetch-movie`);
     const movies = await response.json();
+    const data = movies.results[random];
 
-    document.body.style.backgroundImage = `linear-gradient(to bottom right, rgba(19, 20, 43, 0.6), rgba(0, 0, 0, 0.8)), url(${imgPath}${movies.results[8].backdrop_path})`;
+    const genre = data.genre_ids
+      .map((id) => {
+        for (const [key, value] of Object.entries(genreID)) {
+          if (id === value) {
+            if (key.includes("_")) {
+              const split = key.split("_");
+              return split.map((word) => firstLetterCapitalize(word)).join(" ");
+            } else {
+              return firstLetterCapitalize(key);
+            }
+          }
+        }
+      })
+      .join(", ");
+
+    document.body.style.backgroundImage = `linear-gradient(to bottom right, rgba(13, 16, 24, 0.8), rgba(0, 0, 0, 0.9)), url(${imgPath}${data.backdrop_path})`;
+
+    featuredTitle.textContent = data.title;
+    featuredDate.textContent = data.release_date.slice(0, 4);
+    featuredGenre.textContent = genre;
+    featuredOverview.textContent = data.overview;
   } catch (error) {
     console.log(error);
   }
 };
 
-loadBackground();
+loadInitial();
